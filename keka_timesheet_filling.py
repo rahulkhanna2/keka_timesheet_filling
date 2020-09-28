e90343522b9e19bf6c85a545995e4705c916c79d
Rahul Khanna <khanna_r15@hotmail.com>
2020-09-28 16:16:30 +0530

    Adding Check for Not Null Description and Changing request body

--- keka_timesheet_filling.py
+++ keka_timesheet_filling.py
@@ -30,35 +30,40 @@ task_id = config.get("PROJECT_DETAILS", "TASK_ID")
 project_id = config.get("PROJECT_DETAILS", "PROJECT_ID")
 cookie = config.get("DETAILS", "COOKIE")
 
-
 df = pd.read_csv(str(filepath))
 df['Date'] = pd.to_datetime(df['Date'])
-new_df = df[df["Date"].between(start_date,end_date)]
+new_df = df[df["Date"].between(start_date, end_date)]
+headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "content-type": "application/json;charset=UTF-8",
+    "cookie": cookie,
+    "dnt": "1",
+    "origin": "https://knoldus.keka.com",
+    "referer": "https://knoldus.keka.com/",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
+    "x-requested-with": "XMLHttpRequest"
+}
 
 for index, row in new_df.iterrows():
     previous_date = (row["Date"] - timedelta(1)).strftime('%Y-%m-%dT18:30:00.000Z')
     current_date = str(row['Date'].to_pydatetime().date())
-    url = 'https://knoldus.keka.com/api/mytime/timesheet'
-    data = json.dumps({"timesheet": {"startDate": str(previous_date)},
-                       "timesheetEntries": [
-                           {"date": str(current_date), "projectId": project_id, "taskId": task_id, "startTime": "10:45",
-                            "endTime": "18:45", "totalHours": 9, "comments": str(row.Description), "billable": "true",
-                            "billingClassificationId": billing_id, "sequenceNumber": 1}]})
-    print("------------------------------------")
-    headers = {
-        "accept": "application/json, text/plain, */*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-        "content-type": "application/json;charset=UTF-8",
-        "cookie": (cookie),
-        "dnt": "1",
-        "origin": "https://knoldus.keka.com",
-        "referer": "https://knoldus.keka.com/",
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
-        "x-requested-with": "XMLHttpRequest"
-    }
-    res = requests.post(url=url, data=data, headers=headers)
-    if (res.status_code == 200):
-        print("Successfull for date: " + str(current_date))
+    if pd.notna(row["Description"]):
+        url = 'https://knoldus.keka.com/api/mytime/timesheet'
+        data = json.dumps({"timesheet": {"startDate": str(previous_date), "endDate": str(current_date)},
+                           "timesheetEntries": [
+                               {"date": str(current_date), "projectId": project_id,
+                                "taskId": task_id, "startTime": "10:45",
+                                "endTime": "18:45", "totalHours": 9,
+                                "comments": str(row.Description), "billable": "true",
+                                "billingClassificationId": billing_id, "sequenceNumber": 1}]})
+        res = requests.post(url=url, data=data, headers=headers)
+        print("-------------------------------------------")
+        if res.status_code == 200:
+            print("Successful for date: " + str(current_date))
+        else:
+            print("Unsuccessful for date: " + str(current_date))
     else:
-        print("Unsuccessfull for date: " + str(current_date))
+        print("*****************************************")
+        print("Skipping for date: " + str(current_date))
